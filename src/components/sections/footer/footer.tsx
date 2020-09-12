be15b623dd8966c836cea161f0021ebb07b98532
lmarzy <me@phz3.net>
2020-09-12 18:33:25 +0100

    Initial commit

---
+++ src/components/sections/footer/footer.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+import { Container } from '../../shared';
+
+export const Footer = (): ReactElement => (
+  <footer className="c-footer">
+    <Container>
+      <small>&copy; 2020 - React</small>
+    </Container>
+  </footer>
+);
---
+++ src/components/sections/header/header.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+import { Container } from '../../shared';
+
+export const Header = (): ReactElement => (
+  <header className="c-header">
+    <Container>
+      <h1>React</h1>
+    </Container>
+  </header>
+);
